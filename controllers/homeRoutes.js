f37fa0fa932b60703ddc963c00eed638d2efd3a9
jmcdlungren <j.mcd.lungren@gmail.com>
2023-03-30 13:51:00 -0700

    working on routes, handlebars

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require("../models");
+
+router.get("/", async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [User]
+        })
+        const posts = postData.map((post) => post.get({
+            plain:true
+        }))
+        // res.json(posts)
+        
+    } catch(err) {
+        res.status(400).json(err)
+    }
+})
+
+module.exports = router;
