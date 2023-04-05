66bea09b1b6ba8ee844a18920e96dd7290d355f6
jmcdlungren <j.mcd.lungren@gmail.com>
2023-04-05 14:29:21 -0700

    working on post handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,45 +7,51 @@ router.get("/", async (req, res) => {
             include: [User, Comment]
         })
         const posts = postData.map((post) => post.get({
-            plain:true
+            plain: true
         }))
         // res.json(posts)
-        res.render("homepage", {posts})
-    } catch(err) {
+        res.render("homepage", { posts })
+    } catch (err) {
         res.status(400).json(err)
     }
 })
 
 router.get('/post/:id', async (req, res) => {
     try {
-      const postData = await Post.findByPk(req.params.id, {
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
-      });
-  
-      const post = postData.get({ plain: true });
-  
-      res.render("post", {
-        ...post,
-        logged_in: req.session.logged_in
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                User,
+                {
+                    model: Comment,
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    ]
+                }
+            ],
+        });
 
-  router.get('/login', (req, res) => {
+        const post = postData.get({ plain: true });
+        console.log(post)
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
+        res.redirect('/profile');
+        return;
     }
-  
+
     res.render('login');
-  });
+});
 
 module.exports = router;
